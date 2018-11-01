b0cddd24db00ebad433405ae92dbef0eba3f3e75
GUNAWAN18ID <44337431+GUNAWAN18ID@users.noreply.github.com>
2018-11-01 14:01:03 +0700

    Add files via upload

---
+++ B3604-spam.sh
@@ -0,0 +1,63 @@
+#MAU NGAPAIN MONYET !!
+#MAU RECODE !?
+#HAHAHA KONTOL RECODE AJG !!
+#NGAPAIN MASIH DI SINI MONYET !!
+#PERGI LAGI SANA KONTOL !!
+#BANGSAT LU ANJING !!
+#LU YANG MONYET !!
+#PERGI JAUH JAUH KONTOL BABI !!
+#SALAM HEKEL
+#SALAM MAQLO
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+echo "##################################"|lolcat
+echo "/ TOOLS : SPAM v1                /"|lolcat
+echo "/ CODED : MR.B3604               /"|lolcat
+echo "/ TEAM  : 407 Authentic eXploit  /"|lolcat
+echo "/ youtube : GUNAWAN ID           /"|lolcat
+echo "##################################"|lolcat
+
+echo "daftar tools spam"
+echo "[1] Bom Chat Whatsapp"
+echo "[2] Spam Telpon"
+echo "[3] Keluar"
+read -p "PILIH SALAH SATU : " bro
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+php mdr2.php
+sh B3604-spam.sh
+fi
+
+if [ $bro = 2 ] || [ $bro = 01 ]
+then
+clear
+php mdr1.php
+sh B3604-spam.sh
+fi
+
+if [ $bro = 3 ] || [ $bro = 03 ]
+then
+echo "\033[31;1mWE ARE 407 AUTHENTIC EXPLOIT"
+sleep 1
+echo "\033[32;1mTerimakasih Sudah Menggunakan Tools ini"
+sleep 1
+echo "\033[35;1mSelamat Beraktivitas"
+sleep 1
+exit
+fi
